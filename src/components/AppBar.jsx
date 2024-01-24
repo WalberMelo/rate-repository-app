55c0522c00d97a0d02b03123c86aa7af52783868
Walber Melo <dev4wm@gmail.com>
2024-01-24 18:42:49 +0000

    Feat(route): Add appbar router

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -1,27 +1,42 @@
 import { View, StyleSheet } from "react-native";
+import { Link } from "react-router-native";
 import Constants from "expo-constants";
+
 import theme from "../theme";
 import Text from "./Text";
+import { ScrollView } from "react-native";
 
-let statusbar = Constants.statusBarHeight;
-statusbar = 70;
+let statusBarHeight = Constants.statusBarHeight;
+statusBarHeight = 70;
 
 const styles = StyleSheet.create({
   container: {
-    paddingTop: statusbar,
+    paddingTop: statusBarHeight,
     backgroundColor: theme.colors.appBarColor,
     paddingBottom: 20,
     paddingLeft: 20,
   },
-  // ...
+  link: {
+    marginHorizontal: 10,
+  },
 });
 
 const AppBar = () => {
   return (
     <View style={styles.container}>
-      <Text color="textLight" fontSize="subheading" fontWeight="bold">
-        Repositories
-      </Text>
+      <ScrollView horizontal>
+        <Link to="/" style={styles.link}>
+          <Text color="textLight" fontSize="subheading" fontWeight="bold">
+            Repositories
+          </Text>
+        </Link>
+
+        <Link to="/signin" style={styles.link}>
+          <Text color="textLight" fontSize="subheading" fontWeight="bold">
+            SignIn
+          </Text>
+        </Link>
+      </ScrollView>
     </View>
   );
 };
